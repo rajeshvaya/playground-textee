893ddb4aae9c0f54207bd1d4fb23c142416ed486
rajeshvaya <vaya.rajesh@gmail.com>
2014-11-28 13:11:37 +0400

    stupid text editing

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
+import Tkinter
 from Tkinter import *
-from ttk import *
+from ScrolledText import *
+import tkFileDialog
+import tkMessageBox
 
-# functions
-def close():
-    global root
-    print "Closing the application"
-    root.quit()
+root = Tkinter.Tk(className=" Textee")
+textPad = ScrolledText(root, width=100, height=50)
 
+def open_command():
+    file = tkFileDialog.askopenfile(parent=root,mode='rb',title='Select a file')
+    if file != None:
+            contents = file.read()
+            textPad.insert('1.0',contents)
+            file.close()
 
-# main
-root = Tk()
+def save_command():
+    global textPad
+    file = tkFileDialog.asksaveasfile(mode='w')
+    if file != None:
+        data = textPad.get('1.0', END+'-1c')
+        file.write(data)
+        file.close()
 
-frame1 = Frame(root, width=300, height=300)
-frame1.pack()
+def exit_command():
+    if tkMessageBox.askokcancel("Quit", "Are you sure?"):
+        root.destroy()
 
-label1 = Label(frame1, text='Hello World!',)
-label1.pack()
+def about_command():
+    label = tkMessageBox.showinfo("About", "Textee - A stupid text editor")
 
-button1 = Button(frame1, text='Exit', command=close)
-button1.pack()
+def do_nothing():
+    print "Ok.."
+    
+menu = Menu(root)
+root.config(menu=menu)
+filemenu = Menu(menu)
+menu.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="New", command=do_nothing)
+filemenu.add_command(label="Open...", command=open_command)
+filemenu.add_command(label="Save", command=save_command)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=exit_command)
+helpmenu = Menu(menu)
+menu.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="About...", command=about_command)
 
-# how long to live?
+#
+textPad.pack()
 root.mainloop()
-
-
-
